e2c24a758eaf998f88a1ed7d511439cf6c819c80
maurineb <83240091+maurineb@users.noreply.github.com>
2023-10-03 16:15:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import subprocess
 
 # OPTIONS
 # Path of the image
-# image_path = "rat.png"
+image_path = "rat.png"
 # image_path = "test_star.png"
 # image_path = "test_loop_outside_main.png"
 # image_path = "test_different_loops.png"
 # image_path = "other_test_loop_outside_main.png"
-image_path = "lizzard-9.png"
+# image_path = "lizzard-9.png"
 # image_path = "apple-18.png"
 # Value of epsilon for the skeleton
 epsilon = "1.0"
@@ -38,11 +38,11 @@ commande = "..\\compact-skel-2d-main\\compact-skel-2d-main"
 os.chdir(commande)
 
 # Compute the skeleton
-commande = "cargo run --release -- --imgin ..\\" + image_path + " --imgout ..\\results\\skeleton\\skeleton.png" + " --epsilon " + epsilon + " --nodout ..\\results\\skeleton\\nod.txt --edgout ..\\results\\skeleton\\edg.txt --delout ..\\results\\skeleton\\del.txt --bndout ..\\results\\skeleton\\bnd.txt" 
+commande = "cargo run --release -- --imgin ..\\..\\" + image_path + " --imgout ..\\..\\results\\skeleton\\skeleton.png" + " --epsilon " + epsilon + " --nodout ..\\..\\results\\skeleton\\nod.txt --edgout ..\\..\\results\\skeleton\\edg.txt --delout ..\\..\\results\\skeleton\\del.txt --bndout ..\\..\\results\\skeleton\\bnd.txt" 
 subprocess.run(commande, shell=True)
 
 # Go to the folder for the decomposition
-commande = "..\\bma_decomposition-main\\bma_decomposition-main"
+commande = "..\\..\\bma_decomposition-main\\bma_decomposition-main"
 os.chdir(commande)
 
 # Compute the decomposition
